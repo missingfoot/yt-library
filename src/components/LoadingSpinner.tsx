6637f556b70015ee8d0cafabd1925438f65a78df
James <missingfoot@jamessparkes.com>
2026-08-09 14:44:55 +0100

    Swap loading spinner for the three-square shuffle animation

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,30 +1,25 @@
 export function LoadingSpinner() {
   return (
     <div className="h-screen flex items-center justify-center">
-      <svg stroke="var(--text-dim)" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="48px" height="48px">
-        <g>
-          <circle cx="12" cy="12" r="9.5" fill="none" strokeWidth="3" strokeLinecap="round">
-            <animate
-              attributeName="stroke-dasharray"
-              dur="1.5s"
-              calcMode="spline"
-              values="0 150;42 150;42 150;42 150"
-              keyTimes="0;0.475;0.95;1"
-              keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
-              repeatCount="indefinite"
-            />
-            <animate
-              attributeName="stroke-dashoffset"
-              dur="1.5s"
-              calcMode="spline"
-              values="0;-16;-59;-59"
-              keyTimes="0;0.475;0.95;1"
-              keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
-              repeatCount="indefinite"
-            />
-          </circle>
-          <animateTransform attributeName="transform" type="rotate" dur="2s" values="0 12 12;360 12 12" repeatCount="indefinite" />
-        </g>
+      <svg fill="var(--text-dim)" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="48px" height="48px">
+        <rect x="1" y="1" rx="1" width="10" height="10">
+          <animate id="spinner_c7A9" begin="0;spinner_23zP.end" attributeName="x" dur="0.2s" values="1;13" fill="freeze" />
+          <animate id="spinner_Acnw" begin="spinner_ZmWi.end" attributeName="y" dur="0.2s" values="1;13" fill="freeze" />
+          <animate id="spinner_iIcm" begin="spinner_zfQN.end" attributeName="x" dur="0.2s" values="13;1" fill="freeze" />
+          <animate id="spinner_WX4U" begin="spinner_rRAc.end" attributeName="y" dur="0.2s" values="13;1" fill="freeze" />
+        </rect>
+        <rect x="1" y="13" rx="1" width="10" height="10">
+          <animate id="spinner_YLx7" begin="spinner_c7A9.end" attributeName="y" dur="0.2s" values="13;1" fill="freeze" />
+          <animate id="spinner_vwnJ" begin="spinner_Acnw.end" attributeName="x" dur="0.2s" values="1;13" fill="freeze" />
+          <animate id="spinner_KQuy" begin="spinner_iIcm.end" attributeName="y" dur="0.2s" values="1;13" fill="freeze" />
+          <animate id="spinner_arKy" begin="spinner_WX4U.end" attributeName="x" dur="0.2s" values="13;1" fill="freeze" />
+        </rect>
+        <rect x="13" y="13" rx="1" width="10" height="10">
+          <animate id="spinner_ZmWi" begin="spinner_YLx7.end" attributeName="x" dur="0.2s" values="13;1" fill="freeze" />
+          <animate id="spinner_zfQN" begin="spinner_vwnJ.end" attributeName="y" dur="0.2s" values="13;1" fill="freeze" />
+          <animate id="spinner_rRAc" begin="spinner_KQuy.end" attributeName="x" dur="0.2s" values="1;13" fill="freeze" />
+          <animate id="spinner_23zP" begin="spinner_arKy.end" attributeName="y" dur="0.2s" values="1;13" fill="freeze" />
+        </rect>
       </svg>
     </div>
   );
